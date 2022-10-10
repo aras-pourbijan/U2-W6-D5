007af53af1002b75debbfe74cdf49ba619ce63a3
Aras Pourbijan <arasmidos@gmail.com>
2022-10-10 09:01:37 +0200

    unday

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let changeTheme = function () {
+    document.querySelector(".navbar").classList.toggle("navbar-dark");
+
+    document.querySelector(".container").classList.toggle("bg-dark", "text-dark");
+    console.log("hello");
+};
