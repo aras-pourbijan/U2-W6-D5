16a01077ee135519adf704601f7550541b61d0bb
Aras Pourbijan <arasmidos@gmail.com>
2022-10-10 20:07:22 +0200

    modificato!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 let changeTheme = function () {
-    document.querySelector(".navbar").classList.toggle("navbar-dark");
+    const allNav = document.querySelectorAll("nav");
+    const bodyCss = document.querySelector(".container-fluid");
+    const logos = document.querySelector("#partner-logo");
+    bodyCss.classList.toggle("bg-dark");
+    bodyCss.classList.toggle("text-light");
 
-    document.querySelector(".container").classList.toggle("bg-dark", "text-dark");
-    console.log("hello");
+    for (const nav of allNav) {
+        nav.classList.toggle("navbar-dark");
+        nav.classList.toggle("navbar-light");
+        nav.classList.toggle("bg-light");
+        nav.classList.toggle("bg-dark");
+    }
 };
